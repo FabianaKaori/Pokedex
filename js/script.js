974b948cdeb72ba4baefd8269489d5b1a206b8c2
Fabiana Kaori <fabianakaori.93@gmail.com>
2022-10-12 02:36:57 -0300

    Correção fonte ultrapassando limite, bug imagem

--- js/script.js
+++ js/script.js
@@ -5,12 +5,14 @@ const pokemonImage = document.querySelector('.pokemon__image');
 const form = document.querySelector('.form');
 const input = document.querySelector('.input__search');
 const buttonPrev = document.querySelector('.btn-prev');
-const buttonNext = document.querySelector('.btn-Next');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
-    const APIResponse = await fetch(`http://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+    const APIResponse = await fetch(`http://pokeapi.co/api/v2/pokemon/${pokemon}`);
     
-        if (APIResponse.status == 200){
+        if (APIResponse.status === 200) {
         const data = await APIResponse.json();
         return data;
     }
@@ -24,18 +26,18 @@ const renderPokemon = async (pokemon) => {
     const data = await fetchPokemon(pokemon);
 
     if(data){
-
+        pokemonImage.style.display = 'block';
         pokemonName.innerHTML = data.name;
         pokemonNumber.innerHTML = data.id;
-        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-    
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];    
         input.value = '';
-    }else{
-        pokemonImage.getElementsByClassName.display = 'none'
+        searchPokemon = data.id;
+    } else{
+        pokemonImage.style.display = 'none'
         pokemonName.innerHTML = 'Não encontrado' ;
         pokemonNumber.innerHTML = '';
     }
-    }
+}
     
   
 
@@ -45,13 +47,15 @@ form.addEventListener('submit', (event) => {
 });
 
 buttonPrev.addEventListener('click', () => {
-    alert('prev clicked')
-
+    if (searchPokemon > 1) {
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+    }
 });
 
 buttonNext.addEventListener('click', () => {
-    alert('next clicked')
-
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
 });
 
-renderPokemon('searchPokemon')
+renderPokemon(searchPokemon)
